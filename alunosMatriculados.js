08462b0c413776b3800bb2e3bbdd673c92d196f0
bpoltosi <brunopoltosi@gmail.com>
2026-08-21 23:51:05 -0300

    Aprimorar mensagens de console na função implementaRelacoes para melhor clareza

--- alunosMatriculados.js
+++ alunosMatriculados.js
@@ -2,7 +2,7 @@ function implementaRelacoes(d1,d2,d3){
 
     //matriculados em D1, D2 e D3
     let todos = new Set([...d1, ...d2, ...d3]);
-    console.log("todos matriculados em alguma turma: ");
+    console.log("\nTodos alunos matriculados em alguma turma: ");
     console.log(todos);
 
     //matriculados apenas em D1
@@ -13,12 +13,13 @@ function implementaRelacoes(d1,d2,d3){
     let D1menosD2 = new Set(Array.from(D1).filter(x => !D2.has(x)));
     let D1menosD2menosD3 = new Set(Array.from(D1menosD2).filter(x => !D3.has(x)));
 
-    console.log('Matriculados so em D1: ');
+    console.log('\nMatriculados so em D1: ');
     console.log(D1menosD2menosD3);
 
     let interseccaoD1D2 = new Set(Array.from(D1).filter(x => D2.has(x)));
 
-    console.log("Matriculados simultaneamente em D1 e D2");
+    //matriculados em D1 e D2
+    console.log("\nMatriculados simultaneamente em D1 e D2: ");
     console.log(interseccaoD1D2);
 }
 
@@ -26,4 +27,4 @@ let alunosD1 = ['jorge','luis','marcia','janete','carla','rafael','melina'];
 let alunosD2 = ['luis','marcia','marcelo','janete','mariana','carla','rafael'];
 let alunosD3 = ['luis','marcia','janete','mariana'];
 
-implementaRelacoes(ld1,ld2,l3);
+implementaRelacoes(alunosD1,alunosD2,alunosD3);
